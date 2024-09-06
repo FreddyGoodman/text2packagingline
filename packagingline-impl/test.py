a992683c6a1d71444346d3784e37a2c63f60bab7
Freddy Goodman <freddygoodman0601@gmail.com>
2024-09-06 15:25:07 +0200

    tweaked zero shot promts

--- packagingline-impl/test.py
+++ packagingline-impl/test.py
@@ -14,9 +14,9 @@ for i in range(n):
 
 
 # Add conveyor belts to connect the submachines
-packaging_line.add_conveyor_belt(beginning=0, end=n - 1, type="item")
-packaging_line.add_conveyor_belt(beginning=0, end=n - 1, type="item")
-packaging_line.add_conveyor_belt(beginning=0, end=n - 1, type="tray")
+packaging_line.add_conveyor_belt(beginning=0, end=n - 1, belt_type="item")
+packaging_line.add_conveyor_belt(beginning=0, end=n - 1, belt_type="item")
+packaging_line.add_conveyor_belt(beginning=0, end=n - 1, belt_type="tray")
 
 packaging_line.submachines[0].attach_funnel()
 packaging_line.submachines[1].attach_funnel()
@@ -24,5 +24,32 @@ packaging_line.submachines[2].attach_funnel()
 packaging_line.submachines[3].attach_funnel()
 
 
+print(packaging_line.throughput_string())
+print(packaging_line.cost_string())
+
+# Step 1: Initialize the packaging line with 4 sub-machines
+packaging_line = PackagingLine(n=4)
+
+# Step 2: Add conveyor belts
+# Conveyor belt for items
+packaging_line.add_conveyor_belt(beginning=0, end=3, belt_type='item')
+
+# Conveyor belt for trays
+packaging_line.add_conveyor_belt(beginning=0, end=3, belt_type='tray')
+
+# Step 3: Configure sub-machines
+# Add scanners and pickers to each sub-machine
+for submachine in packaging_line.submachines:
+    # Add one "scanner" robot
+    submachine.add_packaging_robot(machine_type='scanner')
+    
+    # Add three "picker" robots (since each submachine can hold up to 4 robots and 1 is a scanner)
+    for _ in range(3):
+        submachine.add_packaging_robot(machine_type='picker')
+    
+    # Attach a funnel to the sub-machine
+    submachine.attach_funnel()
+
+# The PackagingLine is now set up with enough capacity to package 400 items.
 print(packaging_line.throughput_string())
 print(packaging_line.cost_string())
